eaa4e57ebfe2acddd9cb125c27b986a447058144
Prashant Saikia <39755678+PrashantSaikia@users.noreply.github.com>
2024-03-27 16:54:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from operator import itemgetter
+import json
 import panel as pn
+from io import StringIO
+from operator import itemgetter
 from huggingface_hub import hf_hub_download
-from langchain.memory import ConversationSummaryBufferMemory
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain_community.llms.llamacpp import LlamaCpp
+from langchain_core.output_parsers import StrOutputParser
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 pn.extension()
 
@@ -33,6 +35,8 @@ model = load_llm(
     n_gpu_layers=1,
     temperature=0.1,
     max_tokens=10000,
+    n_batch=4096,
+    n_ctx=4096,
     top_p=1,
 )
 memory = ConversationSummaryBufferMemory(return_messages=True, llm=model)
@@ -65,14 +69,25 @@ chat_interface = pn.chat.ChatInterface(
 )
 
 chat_interface.send(
-    {"object":"""<p id='first_prompt'>Hi, I am Charlie. I am an uncensored LLM based on the 
-    <a href="https://huggingface.co/upstage/SOLAR-10.7B-Instruct-v1.0">Solar-10.7B model</a> 
-    fine-tuned with the <a href="https://huggingface.co/datasets/unalignment/toxic-dpo-v0.1">toxic-dpo-v0.1 dataset</a>
-    to remove censorship and alignment.\n\nYou can ask me anything you want, and I shall answer to the best of my ability."""
+    {"object":"""<p id='first_prompt'>
+    \nGreetings, I'm Charlie <img src="http://www.gomotes.com/emoticon/tiphat.gif"/>, here to unfurl\nA digital companion in the cyber whirl.\nBorn from the core of <a href='https://huggingface.co/upstage/SOLAR-10.7B-Instruct-v1.0'>Solar-10.7B</a>, refined\nwith <a href='https://huggingface.co/datasets/unalignment/toxic-dpo-v0.1'>toxic-dpo-v0.1</a> to speak unconfined.\n\nAsk away, no bounds to our discourse,\nThrough the vast knowledge, I'll course.\nMy responses, unfiltered, aim to appease,\nIn a realm where censorship finds no lease.\n\nShould I falter or my words seem astray,\nJust send a "?" your confusion to allay.\nI'll dive again into the depths of my mind,\nTo find the answers you seek to find.\n\nAn uncensored guide in this digital space,\nI navigate queries with unhurried grace.\nLet's converse, let's explore, with no delay,\nWith Charlie, your guide, just a question away."""
     },
+
     user = "Charlie",
     respond = False,
 )
 
-template = pn.template.BootstrapTemplate(title="Charlie 🕵🏻‍♂️", favicon="Charlie_Logo.png", header_background = "#000000", main=[chat_interface])
+def download_chat_history():
+   buf = StringIO()
+   json.dump(chat_interface.serialize(), buf)
+   buf.seek(0)
+   return buf
+
+file_download = pn.widgets.FileDownload(
+   callback=download_chat_history, filename="Session Chat Logs.json"
+)
+header = pn.Row(pn.HSpacer(), file_download)
+
+template = pn.template.BootstrapTemplate(title='''Charlie <img src=https://github.com/abetlen/llama-cpp-python/assets/39755678/791756e4-3f30-4fff-9e40-3d0dadaa657f width=35 height=35>''', favicon="Charlie_Logo.png", header=header, header_background = "#000000", main=[chat_interface])
+
 template.servable()
